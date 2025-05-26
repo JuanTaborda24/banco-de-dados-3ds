8688b6be8a451d1ed1d9161182c32ea1d198d539
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-05-25 22:16:06 -0300

    Update dados-categoryProduct.sql

--- dados-categoryProduct.sql
+++ dados-categoryProduct.sql
@@ -2,14 +2,17 @@
 BEGIN TRANSACTION; 
 
 -- Insere dados na tabela CategoryProducts
-INSERT INTO CategoryProduct VALUES(1,'Beverages','Soft drinks, coffees, teas, beers, and ales');
-INSERT INTO CategoryProduct VALUES(2,'Condiments','Sweet and savory sauces, relishes, spreads, and seasonings');
-INSERT INTO CategoryProduct VALUES(3,'Confections','Desserts, candies, and sweet breads');
-INSERT INTO CategoryProduct VALUES(4,'Dairy Products','Cheeses');
-INSERT INTO CategoryProduct VALUES(5,'Grains/Cereals','Breads, crackers, pasta, and cereal');
-INSERT INTO CategoryProduct VALUES(6,'Meat/Poultry','Prepared meats');
-INSERT INTO CategoryProduct VALUES(7,'Produce','Dried fruit and bean curd');
-INSERT INTO CategoryProduct VALUES(8,'Seafood','Seaweed and fish');
+INSERT INTO CategoryProducts VALUES(1,'Beverages','Soft drinks, coffees, teas, beers, and ales');
+INSERT INTO CategoryProducts VALUES(2,'Condiments','Sweet and savory sauces, relishes, spreads, and seasonings');
+INSERT INTO CategoryProducts VALUES(3,'Confections','Desserts, candies, and sweet breads');
+INSERT INTO CategoryProducts VALUES(4,'Dairy Products','Cheeses');
+INSERT INTO CategoryProducts VALUES(5,'Grains/Cereals','Breads, crackers, pasta, and cereal');
+INSERT INTO CategoryProducts VALUES(6,'Meat/Poultry','Prepared meats');
+INSERT INTO CategoryProducts VALUES(7,'Produce','Dried fruit and bean curd');
+INSERT INTO CategoryProducts VALUES(8,'Seafood','Seaweed and fish');
 
 -- Encerra a transação e salva os dados com segurança
 COMMIT;
+
+-- Consulta tabela CategoryProducts
+SELECT * FROM CategoryProducts;
