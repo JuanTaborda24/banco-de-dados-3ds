8f809198af570dd4882b9e95d59f35e3d8f60c41
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-05-25 23:43:22 -0300

    Update northwind-customers.sql

--- northwind-customers.sql
+++ northwind-customers.sql
@@ -94,8 +94,6 @@ INSERT INTO Customers VALUES(89,'White Clover Markets','Karl Jablonski','305 - 1
 INSERT INTO Customers VALUES(90,'Wilman Kala','Matti Karttunen','Keskuskatu 45','Helsinki','21240','Finland');
 INSERT INTO Customers VALUES(91,'Wolski','Zbyszek','ul. Filtrowa 68','Walla','01-012','Poland');
 
-
-
 -- Encerra a transação e salva os dados com segurança
 COMMIT;
 
