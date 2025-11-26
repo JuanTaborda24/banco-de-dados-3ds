dde86a624eb6569ffa6736cd00d7130deb56a430
JuanTaborda24 <taborda.juan@escola.pr.gov.br>
2025-11-26 11:28:37 -0300

    Create calculaPreco.cs

---
+++ calculaPreco.cs
@@ -0,0 +1,52 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+class Produto
+
+{
+
+    public string Nome { get; set; }
+
+    public double Preco { get; set; }
+
+    public double Desconto { get; set; }
+
+
+    public double CalcularPrecoFinal()
+
+    {
+
+        return Preco - (Preco * Desconto / 100);
+
+    }
+
+}
+
+class Program
+
+{
+
+    static void Main()
+
+    {
+
+        Produto produto = new Produto
+
+        {
+
+            Nome = "Fone de ouvido",
+
+            Preco = 200,
+
+            Desconto = 15
+
+        };
+
+        Console.WriteLine($"Preço final de {produto.Nome}: R$ {produto.CalcularPrecoFinal():F2}");
+
+    }
+
+}
+
